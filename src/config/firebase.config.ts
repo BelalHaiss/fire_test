b33f8d528f10581a8d472b92efe32ddc57e8e681
BelalHaiss <belalmohsen10@gmail.com>
2024-04-27 14:40:06 +0300

    [feat] add eldawly project as staging project

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCnwnQVPuzuKmzoXOmI_SRpD26UlAKnHiM',
+  authDomain: 'fire-test-3cb81.firebaseapp.com',
+  projectId: 'fire-test-3cb81',
+  storageBucket: 'fire-test-3cb81.appspot.com',
+  messagingSenderId: '899134205803',
+  appId: '1:899134205803:web:06bb55a53bd32bef0c262e'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
